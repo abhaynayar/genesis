199c4c3c4b51f3881623ba1ce9b62604455c1964
Abhay Nayar <abhaynayar@gmail.com>
2020-09-19 00:26:47 +0530

    finished week11

--- tools/compiler1_test.sh
+++
@@ -1,8 +0,0 @@
-./TextComparer.sh ../projects/10/ExpressionLessSquare/Main.xml  ../projects/10/ExpressionLessSquare/Main.xyz
-./TextComparer.sh ../projects/10/ExpressionLessSquare/Square.xml  ../projects/10/ExpressionLessSquare/Square.xyz
-./TextComparer.sh ../projects/10/ExpressionLessSquare/SquareGame.xml  ../projects/10/ExpressionLessSquare/SquareGame.xyz
-./TextComparer.sh ../projects/10/Square/Main.xml  ../projects/10/Square/Main.xyz
-./TextComparer.sh ../projects/10/Square/Square.xml  ../projects/10/Square/Square.xyz
-./TextComparer.sh ../projects/10/Square/SquareGame.xml  ../projects/10/Square/SquareGame.xyz
-./TextComparer.sh ../projects/10/ArrayTest/Main.xml ../projects/10/ArrayTest/Main.xyz
-
